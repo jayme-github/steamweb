57a5db4b11d973b589ccb7a7efbba808ec2cc7fb
jayme-github <tuxnet@gmail.com>
2015-07-23 17:10:29 +0200

    add requirements

--- setup.py
+++ setup.py
@@ -2,20 +2,24 @@
 
 from setuptools import setup
 
+with open('requirements.txt', 'r') as infile:
+    requirements = [l.strip() for l in infile.readlines()]
+
 setup(
-    name='steamweb',
-    version='0.1',
-    description='lib to access/use steam web pages (stuff not exposed via API)',
-    author='Jayme',
-    author_email='tuxnet@gmail.com',
-    url='https://github.com/jayme-github/steamweb',
-    packages=['steamweb'],
-    classifiers=[
+    name = 'steamweb',
+    version = '0.1',
+    description = 'lib to access/use steam web pages (stuff not exposed via API)',
+    author = 'Jayme',
+    author_email = 'tuxnet@gmail.com',
+    url = 'https://github.com/jayme-github/steamweb',
+    packages = ['steamweb'],
+    classifiers = [
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
     ],
-    scripts=['demo.py'],
+    install_requires = requirements,
+    scripts = ['demo.py'],
 )

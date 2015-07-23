b9a0c417751e641d80bb1e6209fbe3800dc62d24
jayme-github <tuxnet@gmail.com>
2015-07-23 16:45:31 +0200

    define parser to avoid warnings

--- demo.py
+++ demo.py
@@ -7,5 +7,5 @@ swb = SteamWebBrowser()
 if not swb.logged_in():
     swb.login()
 r = swb.get('https://store.steampowered.com/account/')
-soup = BeautifulSoup(r.content)
+soup = BeautifulSoup(r.content, 'html.parser')
 print 'Yout wallet balance:', soup.find('div', attrs={'class': 'accountData price'}).get_text()

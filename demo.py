805d6495f736b63b7cbae033f5f83d430d69c801
jayme-github <tuxnet@gmail.com>
2015-07-28 21:39:24 +0200

    remove beautifulsoup4 requirement

--- demo.py
+++ demo.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 from __future__ import print_function
+import re
+from sys import version_info
 from steamweb.steamwebbrowser import SteamWebBrowser
-from bs4 import BeautifulSoup
+if version_info.major >= 3:
+    from html.parser import HTMLParser
+else:
+    from HTMLParser import HTMLParser
 
 swb = SteamWebBrowser()
 if not swb.logged_in():
     swb.login()
 r = swb.get('https://store.steampowered.com/account/')
-soup = BeautifulSoup(r.content, 'html.parser')
-print('Yout wallet balance:', soup.find('div', attrs={'class': 'accountData price'}).get_text())
+m = re.search(r'<div class="accountData price">(.*) </div>', r.content)
+print('Yout wallet balance:', HTMLParser().unescape(m.groups()[0]))

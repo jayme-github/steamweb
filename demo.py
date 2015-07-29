dd57c325210d98e99eee809bf981fcc144172530
jayme-github <tuxnet@gmail.com>
2015-07-29 21:38:36 +0200

    Steam account page changed layout

--- demo.py
+++ demo.py
@@ -2,15 +2,15 @@
 from __future__ import print_function
 import re
 from sys import version_info
-from steamweb.steamwebbrowser import SteamWebBrowser
+from steamweb.steamwebbrowser import SteamWebBrowserCfg
 if version_info.major >= 3:
     from html.parser import HTMLParser
 else:
     from HTMLParser import HTMLParser
 
-swb = SteamWebBrowser()
+swb = SteamWebBrowserCfg()
 if not swb.logged_in():
     swb.login()
 r = swb.get('https://store.steampowered.com/account/')
-m = re.search(r'<div class="accountData price">(.*) </div>', r.content)
+m = re.search(r'<a href="http://store.steampowered.com/account/history/">(\S+) </a>', r.content)
 print('Yout wallet balance:', HTMLParser().unescape(m.groups()[0]))
